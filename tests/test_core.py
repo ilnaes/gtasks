9ec52773b405e71a924f12a0810328bfb14afd90
Sean Li <seanli646@gmail.com>
2019-11-10 12:39:53 -0500

    started testing

---
+++ tests/test_core.py
@@ -0,0 +1,14 @@
+import unittest
+import pygtasks.core
+
+
+class TestCore(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(1, 1)
+
+    def test_sub(self):
+        self.assertEqual(2,3)
+
+
+if __name__ == '__main__':
+    unittest.main()
